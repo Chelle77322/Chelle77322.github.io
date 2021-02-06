5fe78a48374b53537c84732ee2a1f7dac310d7bb
Chelle77322 <serenynadarra@gmail.com>
2021-02-06 17:17:56 +1030

    Updated script.js file and adding node.js information for pdf resume download

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,44 +1,18 @@
 $(document).ready(function(){
-    $(document).foundation();
-        function DownloadFile(fileName) {
-            //Set the File URL.
-            var url = "./assets/files/Michelle_Hall_Resume.pdf" + fileName;
- 
-            $.ajax({
-                url: url,
-                cache: false,
-                xhr: function () {
-                    var xhr = new XMLHttpRequest();
-                    xhr.onreadystatechange = function () {
-                        if (xhr.readyState == 2) {
-                            if (xhr.status == 200) {
-                                xhr.responseType = "blob";
-                            } else {
-                                xhr.responseType = "text";
-                            }
-                        }
-                    };
-                    return xhr;
-                },
-                success: function (data) {
-                    //Convert the Byte Data to BLOB object.
-                    var blob = new Blob([data], { type: "application/octetstream" });
- 
-                    //Check the Browser type and download the File.
-                    var isIE = false || !!document.documentMode;
-                    if (isIE) {
-                        window.navigator.msSaveBlob(blob, fileName);
-                    } else {
-                        var url = window.URL || window.webkitURL;
-                        link = url.createObjectURL(blob);
-                        var a = $("<a />");
-                        a.attr("download", fileName);
-                        a.attr("href", link);
-                        $("body").append(a);
-                        a[0].click();
-                        $("body").remove(a);
-                    }
-                }
-            });
-        };
+   
+    function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+      
+        element.style.display = 'none';
+        document.body.appendChild(element);
+      
+        element.click();
+      
+        document.body.removeChild(element);
+      }
+      
+      // Start file download.
+      download("hello.txt","This is the content of my file :)");
     });  
